850df2da6a40b821fb216a1e9444cd0c1cc6ee82
Ishver Nayak <ishver.nayak@viitor.cloud>
2022-12-23 12:56:49 +0530

    test

--- src/app/core/services/toaster.service.ts
+++ src/app/core/services/toaster.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@angular/core';
-import {
-  MatSnackBar,
-  MatSnackBarHorizontalPosition,
-  MatSnackBarVerticalPosition
-} from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
-
 import { environment } from 'src/environments/environment';
 
 @Injectable({
--- src/assets/i18n/en.ts
+++ src/assets/i18n/en.ts
@@ -8,6 +8,18 @@ export const locale = {
       quoteRequestLoginRequired: 'You need to login for a quote request.',
       quoteRequestShipperLoginRequired: 'You need to login as a shipper for a quote request.',
     },
+    generalError: {
+      UnhandledException: 'Unhandled Exception',
+      tooManyRequest: 'Too many request!!!',
+      unprocessableEntity: 'Unprocessable Entity!!!',
+      sessionExpired: 'Your session is no longer active. Please login back.',
+      badRequest: 'Bad Request',
+      unAuthorized: 'Your session has expired. Please login back.',
+      invalidPromoCode: 'Please enter valid promocode.',
+      forbidden403: 'The page you are trying to reach is forbidden.',
+      serviceUnavailable: 'Service Unavailable',
+      general: 'Oops!! Something went wrong. Please try again later.'
+    },
     loginPage: {
       loginTitle: 'Login',
       emailTitle: 'E - mail',
@@ -24,7 +36,9 @@ export const locale = {
       loggingInTitle: 'Logging In',
       passwordMinLengthErrorText: 'Password must be at least 6 characters',
       passwordMaxLengthErrorText: 'Password can be max 16 characters',
-      welcomeToText: 'Welcome to'
+      welcomeToText: 'Welcome to',
+      loggedIn: 'You are successfully logged in.',
+      loggedOut: 'You are successfully logged out.',
     },
     homePage: {
       notLoggedIn: 'We couldn\'t log you in.',
