3d17ddb127a6dc260fb748f80c5d667fa883d599
Ishver Nayak <ishver.nayak@viitor.cloud>
2022-12-30 17:56:18 +0530

    user info fixed

--- src/app/pages/events/event-list/event-list.component.ts
+++ src/app/pages/events/event-list/event-list.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
+import { Constants } from 'src/app/core/constants/app.constants';
 import { EventService } from 'src/app/core/services/event.service';
 import { ToasterService } from 'src/app/core/services/toaster.service';
 import { EventListModel } from './../../../core/models/event.model';
@@ -13,11 +14,12 @@ import { EventListModel } from './../../../core/models/event.model';
   templateUrl: './event-list.component.html',
   styleUrls: ['./event-list.component.scss']
 })
-export class EventListComponent implements OnInit {
+export class EventListComponent implements OnInit, OnDestroy {
 
-  displayedColumns = ['title', 'vertical_banner_image', 'is_published', 'created_at'];
+  displayedColumns = ['title', 'vertical_banner_image', 'is_published', 'created_at', 'action'];
   dataSource: MatTableDataSource<EventListModel> | undefined;
 
+  pagination: number[] = Constants.paginationArray;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   filterValue: string = '';
@@ -28,8 +30,8 @@ export class EventListComponent implements OnInit {
     private _router: Router,
     private eventService: EventService,
     private _toasterService: ToasterService,
-
   ) {
+    this.dataSource = new MatTableDataSource();
   }
 
   /**
@@ -45,17 +47,23 @@ export class EventListComponent implements OnInit {
     this.eventService.getEventList().pipe(takeUntil(this.unSubscriber)).subscribe({
       next: (result: any) => {
         if (result && result.data) {
-          this.dataSource = result.data;
-          if (this.dataSource) {
+          if (result.data) {
+            const events = result.data;
+            this.dataSource = new MatTableDataSource(events);
             this.dataSource.paginator = this.paginator;
             this.dataSource.sort = this.sort;
+            console.log('aaabbcc', this.dataSource);
           }
-          console.log('this.eventList', this.dataSource);
+
           // this.dataSource = new MatTableDataSource(this.eventList);
         }
       }
     });
   }
 
+  ngOnDestroy(): void {
+    this.unSubscriber.next();
+    this.unSubscriber.complete();
+  }
 
 }
--- src/assets/i18n/en.ts
+++ src/assets/i18n/en.ts
@@ -69,6 +69,7 @@ export const locale = {
       bannerText: 'Banner',
       statusText: 'Status',
       createdDateText: 'Created Date',
+      actionText: 'Action',
       event: 'Events',
       generalInfo: 'General Info.',
     },
