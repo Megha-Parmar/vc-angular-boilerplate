c7c7675df8b88595245e95e6b0f32abb4eec00d5
Megha Parmar <megha.parmar@viitor.cloud>
2023-08-10 18:55:47 +0530

    fix: updated with proper naming convention

--- src/app/core/constants/app.provide.ts
+++ src/app/core/constants/app.provide.ts
@@ -1,6 +1,6 @@
 import { AdminMockService } from "@mock/admin.mock.service";
 import { AdminService } from "@services/admin.service";
 
-export const serviceProvider = [
+export const mockServiceProvider = [
     { provide: AdminService, useClass: AdminMockService }
 ];
