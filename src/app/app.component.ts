b3862ff3b04175002f1b31309ae52de1da57d89d
Megha Parmar <megha.parmar@viitor.cloud>
2023-09-01 19:02:35 +0530

    feat: integrate snackbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,18 @@ import { UtilityService } from '@services/utility.service';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html'
-})
-export class AppComponent implements OnInit {
+}) export class AppComponent implements OnInit {
+  // @ViewChild(VcHostDirective, { static: true }) vcHost!: VcHostDirective;
 
   constructor(
-    private utilityService: UtilityService
-  ) { }
+    private utilityService: UtilityService,
+    // private service: VcSnackbarService,
+    // private viewContainerRef: ViewContainerRef,
+    // private appRef: ApplicationRef,
+    // private el: ElementRef,
+  ) {
+    // this.service.setRootViewContainerRef(this.viewContainerRef, this.appRef, this.el)
+  }
 
   ngOnInit(): void {
     this.utilityService.setLanguage();
