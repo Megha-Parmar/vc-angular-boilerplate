fb3859735f3b4fdf152123821adb47a148bf5bc8
Megha Parmar <megha.parmar@viitor.cloud>
2023-09-03 18:43:38 +0530

    feat: partner list changes

--- src/app/pages/partner-list/partner-list.component.ts
+++ src/app/pages/partner-list/partner-list.component.ts
@@ -9,12 +9,11 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DialogService } from '@app/core/services/dialog.service';
+import { VcInputComponent } from '@app/shared/vc-libs/vc-input/vc-input.component';
 import {
   COUNTRY_LIST,
   DEBOUNCE_TIME,
   DEFAULT_MAT_DIALOG_CONFIG,
-  DEFAULT_PAGE_INDEX,
-  DEFAULT_PAGE_SIZE,
   MessageType,
   PAGE_SIZE,
   PositionEnum,
@@ -28,26 +27,84 @@ import { AlertToastrService } from '@services/alert-toastr.service';
 import { PartnerService } from '@services/partner.service';
 import { VcEventsService } from '@services/vc-events.service';
 import { VcActionToolbarComponent } from '@vc-libs/vc-action-toolbar/vc-action-toolbar.component';
-import { VcButtonComponent } from '@vc-libs/vc-button/vc-button.component';
-import { VcInputComponent } from '@vc-libs/vc-input/vc-input.component';
 import { VcLoaderComponent } from '@vc-libs/vc-loader/vc-loader.component';
+import {
+  DotToBracketPipe, VcButtonComponent, VcTableBodyDirective, VcTableComponent,
+  VcTableHeaderDirective
+} from 'ng-utility-library';
 import { debounceTime, distinctUntilChanged, finalize } from 'rxjs';
 
 @Component({
   selector: 'app-partner-list',
   standalone: true,
   imports: [CommonModule, MatTableModule, TranslateModule, MatPaginatorModule, MatCheckboxModule,
-    VcButtonComponent, MatIconModule, MatButtonModule, FormsModule, ReactiveFormsModule,
+    VcTableComponent, VcTableBodyDirective, VcTableHeaderDirective,
+    VcButtonComponent, MatIconModule, MatButtonModule, FormsModule, ReactiveFormsModule, DotToBracketPipe,
     NgSelectModule, VcLoaderComponent, VcActionToolbarComponent, VcInputComponent],
   templateUrl: './partner-list.component.html',
   styleUrls: ['./partner-list.component.scss']
 })
 export class PartnerListComponent implements OnInit {
-
+  //   @ContentChild(VcTableHeaderDirective) public tableHeader!: VcTableHeaderDirective;
+  // @ContentChild(VcTableBodyDirective) public tableBody!: VcTableBodyDirective;
   breadcrumbs: BreadCrumb[] = [];
   partnerList = new MatTableDataSource<PartnerDetail>();
   columnLabel = [
     'partnerId', 'companyName', 'street', 'zip', 'city', 'country', 'email', 'phoneNo', 'isActive', 'action'
+  ];
+  page: any = 1;
+
+  columnLabel2 = [
+    { key: 'partnerId', value: 'SR' },
+    { key: 'companyName', value: 'Company Name' },
+    { key: 'street', value: 'Street' },
+    { key: 'zip', value: 'Zip' },
+    { key: 'city', value: 'City' },
+    { key: 'country', value: 'Country' },
+    { key: 'email', value: 'Email' },
+    { key: 'phoneNo', value: 'Phone No' },
+    { key: 'isActive', value: 'Is Active' },
+    { key: 'action', value: '' },
+
+    'partnerId', 'companyName', 'street', 'zip', 'city', 'country', 'email', 'phoneNo', 'isActive', 'action'
+  ];
+
+  // columns = columns;
+  data: any[] = [];// = data;
+
+
+  columns = [
+    //   {
+    //   colName: 'ID',
+    //   fieldName: '_id'
+    // },
+    {
+      colName: 'Company Name',
+      fieldName: 'companyName'
+    },
+    {
+      colName: 'Street',
+      fieldName: 'address.street'
+    },
+    {
+      colName: 'Zip',
+      fieldName: 'address.zip'
+    },
+    {
+      colName: 'City',
+      fieldName: 'address.city'
+    },
+    {
+      colName: 'Country',
+      fieldName: 'address.country'
+    },
+    {
+      colName: 'Email',
+      fieldName: 'email'
+    },
+    { colName: 'Phone No', fieldName: 'phoneNo' },
+    { colName: 'Status', fieldName: 'isActive' },
+    { colName: 'Action', fieldName: 'action', },
   ];
   @ViewChild(MatPaginator) paginator: MatPaginator;
   searchControl = new FormControl('');
@@ -60,6 +117,7 @@ export class PartnerListComponent implements OnInit {
   readonly position = PositionEnum;
 
   private destroyRef = inject(DestroyRef);
+  totalData: number;
 
   constructor(
     private route: ActivatedRoute,
@@ -95,8 +153,8 @@ export class PartnerListComponent implements OnInit {
   getPartnerList(): void {
     const params = {
       sort: this.sortValue.value,
-      pageSize: this.paginator?.pageSize || DEFAULT_PAGE_SIZE,
-      page: (this.paginator?.pageIndex + 1) || DEFAULT_PAGE_INDEX,
+      pageSize: 1,// this.paginator?.pageSize || DEFAULT_PAGE_SIZE,
+      page: this.page,//(this.paginator?.pageIndex + 1) || DEFAULT_PAGE_INDEX,
       ...this.searchValue && { search: this.searchValue }
     };
     this.isLoading = true;
@@ -126,8 +184,13 @@ export class PartnerListComponent implements OnInit {
               }
             ];
           });
+          this.data = res.records;
+          console.log("res.records", res.records);
+          console.log("this.data", this.data);
+
           this.partnerList = new MatTableDataSource(res.records);
           this.paginator.length = res.totalCount;
+          this.totalData = res.totalCount;
         }
       });
   }
@@ -153,6 +216,8 @@ export class PartnerListComponent implements OnInit {
             this.partnerList.data.splice(index, 1);
             this.partnerList = new MatTableDataSource(this.partnerList.data);
             this.paginator.length = this.partnerList.data.length;
+            this.totalData = this.partnerList.data.length;
+
           });
       }
     });
@@ -191,3 +256,100 @@ export class PartnerListComponent implements OnInit {
     this.router.navigate(['../add'], { relativeTo: this.route });
   }
 }
+
+export const columns = [
+  {
+    colName: 'Title',
+    fieldName: 'title'
+  },
+  {
+    colName: 'Name',
+    fieldName: 'name'
+  },
+  {
+    colName: 'Description',
+    fieldName: 'description'
+  }
+];
+
+export const data = [
+  {
+    title: 'Some Mock Title',
+    name: 'Puppy',
+    description: 'Some Mock Description',
+
+  },
+  {
+    title: 'Some Mock Title',
+    name: 'Vinyl Record',
+    description: 'Some Mock Description',
+
+  },
+  {
+    title: 'Some Mock Title',
+    name: 'Watch',
+    description: 'Some Mock Description',
+
+  },
+  {
+    title: 'Some Mock Title',
+    name: 'Clock',
+    description: 'Some Mock Description',
+
+  },
+  {
+    title: 'Some Mock Title',
+    name: 'Leash',
+    description: 'Some Mock Description',
+
+  }, {
+    title: 'Some Mock Title',
+    name: 'Coffee Cup',
+    description: 'Some Mock Description',
+
+  }, {
+    title: 'Some Mock Title',
+    name: 'Bottle',
+    description: 'Some Mock Description',
+  },
+  {
+    title: 'Some Mock Title',
+    name: 'Something else',
+    description: 'Some Mock Description',
+  }
+  , {
+    title: 'Some Mock Title',
+    name: 'Vinyl Record',
+    description: 'Some Mock Description',
+  },
+  {
+    title: 'Some Mock Title',
+    name: 'Vinyl Record',
+    description: 'Some Mock Description',
+  },
+  {
+    title: 'Some Mock Title',
+    name: 'Vinyl Record',
+    description: 'Some Mock Description',
+  },
+  {
+    title: 'Some Mock Title',
+    name: 'Vinyl Record',
+    description: 'Some Mock Description',
+  },
+  {
+    title: 'Some Mock Title',
+    name: 'Vinyl Record',
+    description: 'Some Mock Description',
+  },
+  {
+    title: 'Some Mock Title',
+    name: 'Vinyl Record',
+    description: 'Some Mock Description',
+  },
+  {
+    title: 'Some Mock Title',
+    name: 'Vinyl Record',
+    description: 'Some Mock Description',
+  }
+];
